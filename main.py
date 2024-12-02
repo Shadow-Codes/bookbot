8af3111532b2ff23b0a4b146327989897fcc9c45
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2024-12-02 19:33:18 +0530

    Refactor code structure: removed main function, added try/except block and restructured if statements

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-def main():
-    file_path = "books/frankenstein.txt"
-    text = get_text(file_path)
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_character_count_dict(string):
+    lowered_string = string.lower()
+    character_count = {}
+
+    for char in lowered_string:
+        if not char.isalpha():
+            continue
+
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+
+    return character_count
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
+file_path = ""
+
+try:
+    file_path = input("Enter file path: ").lower()
+
+    with open(file_path) as f:
+        text = f.read()
+
     word_count = get_word_count(text)
     character_count = get_character_count_dict(text)
+
     list_of_dict = []
 
     for k, v in character_count.items():
@@ -22,33 +53,10 @@ def main():
 
     print(f"--- End report ---")
 
-
-def get_text(filepath):
-    with open(filepath) as f:
-        return f.read()
-
-
-def get_word_count(text):
-    words = text.split()
-    return len(words)
-
-
-def get_character_count_dict(string):
-    lowered_string = string.lower()
-    character_count = {}
-
-    for char in lowered_string:
-        if char.isalpha():
-            if char in character_count:
-                character_count[char] += 1
-            else:
-                character_count[char] = 1
-
-    return character_count
-
-
-def sort_on(dict):
-    return dict["count"]
-
-
-main()
+except FileNotFoundError:
+    if file_path == "":
+        print("Please enter the file path")
+    else:
+        print(
+            f"Error: The file '{file_path}' was not found. Please check the file path and try again."
+        )
